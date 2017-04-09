a79d53bad993f2e4c03128465e9c4c5bef591433
Aditya Kamath <adityakamath90@gmail.com>
2017-04-09 22:34:35 +0530

    - Created basic structre for list repo

---
+++ app/src/main/java/com/githubapi/repositoryList/presenter/RepositoryListPresenter.java
@@ -0,0 +1,17 @@
+/*
+ * Created by Aditya on 9/4/17 10:34 PM
+ * Copyright (c) 2017 All rights reserved.
+ *
+ * Last modified 9/4/17 10:34 PM
+ */
+
+package com.githubapi.repositoryList.presenter;
+
+/**
+ * Created by Aditya on 09/04/17.
+ */
+
+public class RepositoryListPresenter {
+
+
+}
---
+++ app/src/main/java/com/githubapi/repositoryList/view/RepositoryList.java
@@ -0,0 +1,22 @@
+/*
+ * Created by Aditya on 9/4/17 10:34 PM
+ * Copyright (c) 2017 All rights reserved.
+ *
+ * Last modified 9/4/17 9:19 PM
+ */
+
+package com.githubapi.repositoryList.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.githubapi.R;
+
+public class RepositoryList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repository_list);
+    }
+}
