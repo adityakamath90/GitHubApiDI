78bf2cbec9b2ccbe52e9f6f8182c620e83acade2
Aditya Kamath <adityakamath90@gmail.com>
2017-04-09 22:36:14 +0530

    - Application class created

---
+++ app/src/main/java/com/githubapi/GithubApiApllication.java
@@ -0,0 +1,18 @@
+/*
+ * Created by Aditya on 9/4/17 10:36 PM
+ * Copyright (c) 2017 All rights reserved.
+ *
+ * Last modified 9/4/17 8:35 PM
+ */
+
+package com.githubapi;
+
+import android.app.Application;
+
+/**
+ * Created by Aditya on 09/04/17.
+ */
+
+public class GithubApiApllication extends Application {
+
+}
