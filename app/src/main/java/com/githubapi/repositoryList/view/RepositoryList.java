8a23ccb1ea01bc4afdead9cdcba15a4c36d065a4
Aditya Kamath <adityakamath90@gmail.com>
2017-04-09 23:29:37 +0530

    - List view added

--- app/src/main/java/com/githubapi/repositoryList/view/RepositoryList.java
+++ app/src/main/java/com/githubapi/repositoryList/view/RepositoryList.java
@@ -1,22 +1,44 @@
 /*
- * Created by Aditya on 9/4/17 10:34 PM
+ * Created by Aditya on 9/4/17 11:29 PM
  * Copyright (c) 2017 All rights reserved.
  *
- * Last modified 9/4/17 9:19 PM
+ * Last modified 9/4/17 11:29 PM
  */
 
 package com.githubapi.repositoryList.view;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.githubapi.R;
+import com.githubapi.repositoryLanguage.model.Item;
+
+import java.util.List;
 
 public class RepositoryList extends AppCompatActivity {
+
+
+    public static final String REPO_LIST = "repoList";
+    private List<Item> mItemList;
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_repository_list);
+        mItemList = getIntent().getParcelableArrayListExtra(REPO_LIST);
+        mListView = (ListView) findViewById(R.id.repoListView);
+        mListView.setAdapter(new RepositoryListAdapter(mItemList, LayoutInflater.from(this)));
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Item item = (Item) adapterView.getItemAtPosition(i);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/githubapi/repositoryList/view/RepositoryListAdapter.java
@@ -0,0 +1,72 @@
+/*
+ * Created by Aditya on 9/4/17 11:29 PM
+ * Copyright (c) 2017 All rights reserved.
+ *
+ * Last modified 9/4/17 11:29 PM
+ */
+
+package com.githubapi.repositoryList.view;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.githubapi.R;
+import com.githubapi.repositoryLanguage.model.Item;
+
+import java.util.List;
+
+/**
+ * Created by Aditya on 09/04/17.
+ */
+
+public class RepositoryListAdapter extends BaseAdapter {
+
+    private List<Item> mItemList;
+    private LayoutInflater mInflater;
+
+    RepositoryListAdapter(List<Item> listItems, LayoutInflater inflater) {
+        mItemList = listItems;
+        mInflater = inflater;
+    }
+
+    @Override
+    public int getCount() {
+        int size = mItemList == null ? 0 : mItemList.size();
+        return size;
+    }
+
+    @Override
+    public Item getItem(int position) {
+        return mItemList.get(position);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup viewGroup) {
+        ViewHolder viewHolder;
+
+        if (view == null) {
+            viewHolder = new ViewHolder();
+            view = LayoutInflater.from(mInflater.getContext()).inflate(R.layout.repositiory_list_item, null);
+            viewHolder.repoItem = (TextView) view.findViewById(R.id.listItem);
+        } else {
+            viewHolder = (ViewHolder) view.getTag();
+        }
+
+        viewHolder.repoItem.setText(mItemList.get(position).getName());
+
+        return view;
+    }
+
+
+    static class ViewHolder {
+        TextView repoItem;
+    }
+}
