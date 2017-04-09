599f14e47b52f929e1d99c535e8f38b21259ad0e
Aditya Kamath <adityakamath90@gmail.com>
2017-04-09 21:21:01 +0530

    - Added config file for Url

---
+++ app/src/main/java/com/githubapi/utils/Config.java
@@ -0,0 +1,18 @@
+/*
+ * Created by Aditya on 9/4/17 9:21 PM
+ * Copyright (c) 2017 All rights reserved.
+ *
+ * Last modified 9/4/17 8:12 PM
+ */
+
+package com.githubapi.utils;
+
+/**
+ * Created by Aditya on 09/04/17.
+ */
+public class Config {
+
+    public static final String BASE_URL = "https://api.github.com/";
+
+    public static final String API_SEARCH_REPOSITORIES = "search/repositories";
+}
