e3b7947f3ebce67fa71318b9d8bf71b71f6e173a
Aditya Kamath <adityakamath90@gmail.com>
2017-04-09 18:34:02 +0530

    - Initial commit

---
+++ app/src/main/java/com/githubapi/LanuageActivity.java
@@ -0,0 +1,13 @@
+package com.githubapi;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LanuageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lanuage);
+    }
+}
