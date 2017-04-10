ed3f011514a5bf8d1ed7a721196038d49bff8a31
Aditya Kamath <adityakamath90@gmail.com>
2017-04-10 15:57:02 +0530

    - Added retrofit interceptor

--- app/src/main/java/com/githubapi/utils/RestClient.java
+++ app/src/main/java/com/githubapi/utils/RestClient.java
@@ -1,12 +1,18 @@
 /*
- * Created by Aditya on 9/4/17 9:21 PM
+ * Created by Aditya on 10/4/17 3:57 PM
  * Copyright (c) 2017 All rights reserved.
  *
- * Last modified 9/4/17 9:21 PM
+ * Last modified 10/4/17 3:57 PM
  */
 
 package com.githubapi.utils;
 
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -18,10 +24,18 @@ public class RestClient {
 
     private static RestClient sRestClient;
     private static Retrofit sRetrofit;
+    private static OkHttpClient sOkHttpClient;
 
     static {
+        sOkHttpClient = new OkHttpClient.Builder().addInterceptor(new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request request = chain.request();
+                return chain.proceed(request);
+            }
+        }).build();
         sRetrofit = new Retrofit.Builder().baseUrl(Config.BASE_URL).addConverterFactory(
-                GsonConverterFactory.create()).build();
+                GsonConverterFactory.create()).client(sOkHttpClient).build();
         sRestClient = new RestClient();
     }
 
