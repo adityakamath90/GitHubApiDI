d019480b98117f34e6ebcab997f1db72c61f6ad7
Aditya Kamath <adityakamath90@gmail.com>
2017-04-10 22:51:21 +0530

    - Refactoring

--- app/src/test/java/com/githubapi/repositoryLanguage/LanguagePresenterTest.java
+++ app/src/test/java/com/githubapi/repositoryLanguage/LanguagePresenterTest.java
@@ -1,8 +1,8 @@
 /*
- * Created by Aditya on 10/4/17 10:50 PM
+ * Created by Aditya on 10/4/17 10:51 PM
  * Copyright (c) 2017 All rights reserved.
  *
- * Last modified 10/4/17 10:50 PM
+ * Last modified 10/4/17 10:51 PM
  */
 
 package com.githubapi.repositoryLanguage;
@@ -23,31 +23,31 @@ public class LanguagePresenterTest {
     private static final String JAVA = "java";
     private LanguagePresenter mLanguagePresenter;
     @Mock
-    private LanguageView langauegeView;
+    private LanguageView mLanguageView;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-        mLanguagePresenter = new LanguagePresenter(langauegeView);
+        mLanguagePresenter = new LanguagePresenter(mLanguageView);
     }
 
 
     @Test
     public void testLanguageStringValid() {
         mLanguagePresenter.isLanguageStringValid(JAVA);
-        Mockito.verify(langauegeView).moveToLanguageDetailScreen();
+        Mockito.verify(mLanguageView).moveToLanguageDetailScreen();
     }
 
     @Test
     public void testLanguageStringInValid() {
         mLanguagePresenter.isLanguageStringValid(null);
-        Mockito.verify(langauegeView).displayMessage();
+        Mockito.verify(mLanguageView).displayMessage();
     }
 
 
     @After
     public void tearDown() {
-        langauegeView = null;
+        mLanguageView = null;
         mLanguagePresenter = null;
     }
 
