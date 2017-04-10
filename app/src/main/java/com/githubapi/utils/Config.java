0aac41e41d217c328279bbc7b539bbf6802349d4
Aditya Kamath <adityakamath90@gmail.com>
2017-04-10 19:00:21 +0530

    - Config api endpoint changed

--- app/src/main/java/com/githubapi/utils/Config.java
+++ app/src/main/java/com/githubapi/utils/Config.java
@@ -1,8 +1,8 @@
 /*
- * Created by Aditya on 10/4/17 5:00 PM
+ * Created by Aditya on 10/4/17 7:00 PM
  * Copyright (c) 2017 All rights reserved.
  *
- * Last modified 10/4/17 4:08 PM
+ * Last modified 10/4/17 6:15 PM
  */
 
 package com.githubapi.utils;
@@ -16,7 +16,14 @@ public class Config {
 
     public static final String API_SEARCH_REPOSITORIES = "search/repositories";
 
+    //Query param for Search repo api
     public static final String QUERY = "q";
     public static final String QUERY_PARAM_LANGUAGE = "language:";
     public static final String PAGE = "page";
+
+    //Query param for  issues repo api
+    public static final String ISSUES = "repos/%s/issues";
+    public static final String CONTRIBUTORS = "repos/%s/contributors";
+
+
 }
